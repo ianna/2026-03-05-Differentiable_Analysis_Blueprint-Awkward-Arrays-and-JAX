9bcc0c9590da95804bd124d53ffcee0a9829e287
Ianna Osborne <ianna.osborne@cern.ch>
2026-03-02 17:19:45 +0100

    add comments

--- examples/ragged_jacobian.py
+++ examples/ragged_jacobian.py
@@ -1,9 +1,11 @@
 import awkward as ak
 from juliacall import Main as jl
 
+# 1. Setup
 jl.seval("using AwkwardArray, ForwardDiff")
 
-# Map the jacobian over the data
+# 2. Differentiate inside the map
+# We map the jacobian over the data
 jl.seval("""
 function get_ragged_jacobian(p, data)
     # For each sub-list 'x' in our ragged data:
@@ -14,15 +16,19 @@ function get_ragged_jacobian(p, data)
 end
 """)
 
+# 3. Data & Params
 data_py = ak.Array([[10.1, 20.2], [], [30.3]])
 params_py = [1.5, 2.0, 3.5]
 
+# 4. Conversion (zero-copy)
 jl_data = jl.AwkwardArray.Array(data_py)
 jl_params = jl.convert(jl.Vector[jl.Float64], params_py)
 
+# 5. Call our specialized function
 # This returns a list of matrices (one matrix per sub-list)
 jac_jl = jl.get_ragged_jacobian(jl_params, jl_data)
 
+# 6. Back to Python (copy) (for zero-copy -- see AwkwardArray.jl tutorial)
 jac_py = ak.from_iter(jac_jl)
 print("--- Final Ragged Jacobian ---")
 print(jac_py)
